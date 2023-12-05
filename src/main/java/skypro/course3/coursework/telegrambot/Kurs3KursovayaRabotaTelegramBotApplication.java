a2bd29e9e03db4ab52caca96623417ec6478a9e2
chern <chernenkij.nikitych@gmail.com>
2023-12-05 22:01:28 +0100

    Курс 3 Курсовая работа телеграм бот - изначальный комментарий

---
+++ src/main/java/skypro/course3/coursework/telegrambot/Kurs3KursovayaRabotaTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package skypro.course3.coursework.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs3KursovayaRabotaTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs3KursovayaRabotaTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/course3/coursework/telegrambot/Kurs3KursovayaRabotaTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.course3.coursework.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs3KursovayaRabotaTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
